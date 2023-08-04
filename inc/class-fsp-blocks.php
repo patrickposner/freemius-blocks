cf454aa5536b34df826ae88bab8ef703f8696bca
Patrick Posner <hello@patrickposner.dev>
2023-08-04 15:50:27 +0200

    finshed buy button block

--- inc/class-fsp-blocks.php
+++ inc/class-fsp-blocks.php
@@ -42,7 +42,9 @@ class Blocks {
 	 */
 	public function register_blocks(): void {
 		// Register the block.
-		register_block_type( FBP_PATH . '/build/buy-button' );
+		register_block_type( FBP_PATH . '/build/buy-button', array(
+			'render_callback' => array( $this, 'render_buy_button' ),
+		) );
 	}
 
 	/**
@@ -52,8 +54,8 @@ class Blocks {
 	 */
 	public function register_scripts(): void {
 		// Buy Button Block
-		$fbp_buy_button_asset_file = include( plugin_dir_path( __FILE__ ) . 'build/buy-button/index.asset.php' );
-		wp_register_script( 'buy-button-script', plugins_url( 'build/buy-button/index.js', __FILE__ ), $fbp_buy_button_asset_file['dependencies'], $fbp_buy_button_asset_file['version'] );
+		$fbp_buy_button_asset_file = include( FBP_PATH . '/build/buy-button/index.asset.php' );
+		wp_register_script( 'buy-button-script', FBP_URL . '/build/buy-button/index.js', $fbp_buy_button_asset_file['dependencies'], $fbp_buy_button_asset_file['version'] );
 	}
 
 	/**
@@ -62,18 +64,55 @@ class Blocks {
 	 * @return void
 	 */
 	function add_block_editor_assets(): void {
-		$fbp_buy_button_asset_file = include( plugin_dir_path( __FILE__ ) . 'build/buy-button/index.asset.php' );
+		$fbp_buy_button_asset_file = include( FBP_PATH . '/build/buy-button/index.asset.php' );
 
-		wp_enqueue_script( 'buy-button-script', plugins_url( 'build/buy-button/index.js', __FILE__ ), $fbp_buy_button_asset_file['dependencies'], $fbp_buy_button_asset_file['version'] );
-		wp_enqueue_style( 'buy-button-style', plugins_url( 'build/buy-button/index.css', __FILE__ ) );
+		wp_enqueue_script( 'buy-button-script', FBP_URL . '/build/buy-button/index.js', $fbp_buy_button_asset_file['dependencies'], $fbp_buy_button_asset_file['version'] );
+		wp_enqueue_style( 'buy-button-style', FBP_URL . '/build/buy-button/index.css' );
 
 		// Make the blocks translatable.
 		if ( function_exists( 'wp_set_script_translations' ) ) {
-			wp_set_script_translations( 'buy-button-script', 'freemius-blocks', plugin_dir_path( __FILE__ ) . 'languages' );
+			wp_set_script_translations( 'buy-button-script', 'freemius-blocks', FBP_PATH . '/languages' );
 		}
 	}
 
 	function add_block_frontend_styles(): void {
-		wp_enqueue_style( 'buy-button-style', plugins_url( 'build/buy-button/index.css', __FILE__ ) );
+		wp_enqueue_style( 'buy-button-style', FBP_URL . '/build/buy-button/index.css' );
+	}
+
+	/**
+	 * Server-Side render for Freemius buy button.
+	 *
+	 * @param $attributes
+	 *
+	 * @return string
+	 */
+	public function render_buy_button( $attributes ): string {
+		ob_start();
+		?>
+        <p>
+            <button class="wp-block-button__link wp-element-button <?php echo esc_html( $attributes['className'] ); ?>"
+                    id="freemius-buy-<?php echo esc_html( $attributes['plugin_id'] ); ?>-<?php echo esc_html( $attributes['plan_id'] ); ?>-<?php echo esc_html( $attributes['billing_cycle'] ); ?>">
+				<?php echo esc_html( $attributes['buttonLabel'] ); ?>
+            </button>
+        </p>
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+        <script src="https://checkout.freemius.com/checkout.min.js"></script>
+        <script>
+            let handler = FS.Checkout.configure({
+                plugin_id: '<?php echo esc_html( $attributes['plugin_id'] ); ?>',
+                plan_id: '<?php echo esc_html( $attributes['plan_id'] ); ?>',
+                public_key: '<?php echo esc_html( $attributes['public_key'] ); ?>',
+            });
+
+            $('#freemius-buy-<?php echo esc_html( $attributes['plugin_id'] ); ?>-<?php echo esc_html( $attributes['plan_id'] ); ?>-<?php echo esc_html( $attributes['billing_cycle'] ); ?>').on('click', function (e) {
+                handler.open({
+                    name: 'Passster',
+                    billing_cycle: '<?php echo esc_html( $attributes['billing_cycle'] ); ?>',
+                });
+                e.preventDefault();
+            });
+        </script>
+		<?php
+		return ob_get_clean();
 	}
 }
